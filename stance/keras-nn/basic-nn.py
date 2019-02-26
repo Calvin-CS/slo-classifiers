874a9c2bc2709054b6b3f2ef93311cba96283ef6
rgadams <roy.g.adams@gmail.com>
2019-02-26 20:46:06 +0000

    Changed inputs to NN

--- stance/keras-nn/basic-nn.py
+++ stance/keras-nn/basic-nn.py
@@ -1,26 +1,41 @@
+import numpy
+import pandas
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
 
-iris = datasets.load_iris()
+# load dataset
+dataframe = pandas.read_csv("iris.csv")
+dataset = dataframe.values
+X = dataset[:,0:4].astype(float)
+Y = dataset[:,4]
 
-model = Sequential()
-model.add(Dense(units=8, activation='relu', input_dim=2))
-model.add(Dense(units=3, activation='softmax'))
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
 
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+# convert integers to dummy variables (i.e. one hot encoded)
+dummy_y = np_utils.to_categorical(encoded_Y)
 
-X = iris.data[:, :2]
-Y = iris.target
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(8, input_dim=4, activation='relu'))
+	model.add(Dense(3, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=0)
 
-model.fit(x_train, y_train, epochs=5, batch_size=32)
+kfold = KFold(n_splits=10, shuffle=True)
 
-loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-
-print(model.metrics_names)
-print(loss_and_metrics)
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
