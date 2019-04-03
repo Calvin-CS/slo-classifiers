f6176c11ebe95fe9f183a3a36a3b088bd425d023
rgadams <roy.g.adams@gmail.com>
2019-04-03 09:55:49 -0400

    Added more layers

--- stance/keras-nn/basic-nn.py
+++ stance/keras-nn/basic-nn.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
 from sklearn.model_selection import cross_val_score
@@ -14,59 +15,72 @@ from fire import Fire
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def main(trainset=None, testset=None):
-	# load dataset
-	full_training_dataset = pd.read_csv(trainset)
 
-	full_training_dataset.stance = pd.Categorical(full_training_dataset.stance, categories=["for", "against", "neutral"], ordered=True)
-	full_training_dataset['code'] = full_training_dataset.stance.cat.codes
+def create_model(optimizer='adam', row1=100, row2=50, row3=10):
+    model = Sequential()
+
+    model.add(Dense(50, input_dim=18830, activation='relu'))
+    model.add(Dense(80, activation='tanh'))
+    model.add(Dense(100, activation='tanh'))
+    model.add(Dense(row1, activation='tanh'))
+    model.add(Dense(row2, activation='tanh'))
+    model.add(Dense(row3, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+
+    # Compile model
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return model
 
 
-#	full_training_dataset['combined_t'] = full_training_dataset['tweet_t'] + " " + full_training_dataset['user_description']
-	training_data = full_training_dataset['tweet_t']
-	training_labels = full_training_dataset['code']
+def main(trainset=None, testset=None, output=None):
+    # load dataset
+    full_training_dataset = pd.read_csv(trainset)
 
-	full_testing_dataset = pd.read_csv(testset)
+    full_training_dataset.stance = pd.Categorical(full_training_dataset.stance, categories=["for", "against", "neutral"], ordered=True)
+    full_training_dataset['code'] = full_training_dataset.stance.cat.codes
 
-	full_testing_dataset.stance = pd.Categorical(full_testing_dataset.stance, categories=["for", "against", "neutral"], ordered=True)
-	full_testing_dataset['code'] = full_testing_dataset.stance.cat.codes
+    full_training_dataset['combined_t'] = full_training_dataset['tweet_t'] + ' ' + full_training_dataset['user_description']
+    training_data = full_training_dataset['combined_t']
+    training_labels = full_training_dataset['code']
 
-#	full_testing_dataset['combined_t'] = full_testing_dataset['tweet_t'] + "_" + full_testing_dataset['user_description']
-	testing_data = full_testing_dataset['tweet_t']
-	testing_labels = full_testing_dataset['code']
+    full_testing_dataset = pd.read_csv(testset)
 
-	testing_labels = pd.Categorical(testing_labels)
+    full_testing_dataset.stance = pd.Categorical(full_testing_dataset.stance, categories=["for", "against", "neutral"], ordered=True)
+    full_testing_dataset['code'] = full_testing_dataset.stance.cat.codes
 
-	# Set the vectorizer to transform the data into inputs for classifiers
-	vectorizer = TfidfVectorizer(ngram_range=(1,3), analyzer='char', use_idf=False)
-	x_train = vectorizer.fit_transform(training_data)
-	x_test = vectorizer.transform(testing_data)
+    full_testing_dataset['combined_t'] = full_testing_dataset['tweet_t'] + ' ' + full_testing_dataset['user_description']
+    testing_data = full_testing_dataset['combined_t']
+    testing_labels = full_testing_dataset['code']
 
-	x = np.array([(x, y) for x in range(2, 10, 2) for y in range(2, 10, 2)])
-	df_model = pd.DataFrame(x, columns=['layer1', 'layer2'])
+    testing_labels = pd.Categorical(testing_labels)
 
-	df_model['loss'] = 0.0
-	df_model['acc'] = 0.0
+    # Set the vectorizer to transform the data into inputs for classifiers
+    vectorizer = TfidfVectorizer(ngram_range=(1,3), analyzer='char', use_idf=False)
+    x_train = vectorizer.fit_transform(training_data.values.astype('U'))
+    x_test = vectorizer.transform(testing_data.values.astype('U'))
 
-	for index, row in df_model.iterrows():
-		# create model
-		model = Sequential()
-		model.add(Dense(10, input_dim=12698, activation='relu'))
-		model.add(Dense(row['layer1'].astype(int), activation='tanh'))
-		model.add(Dense(row['layer2'].astype(int), activation='tanh'))
-		model.add(Dense(3, activation='softmax'))
+    model = KerasClassifier(build_fn=create_model)
 
-		# Compile model
-		model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    x = np.array([(x, y, z) for x in [20, 50, 100] for y in [6, 10, 20] for z in [4, 8, 16]])
+    #x = np.array([(x, y, z) for x in [20] for y in [6] for z in [4]])
+    df_model = pd.DataFrame(x, columns=['layer0', 'layer1', 'layer2'])
 
-		model.fit(x_train, training_labels, epochs=15, batch_size=128)
+    df_model['loss'] = 0.0
+    df_model['acc'] = 0.0
 
-		loss_and_metrics = model.evaluate(x_test, testing_labels, batch_size=128)
+    for index, row in df_model.iterrows():
+        # create model
+        model = create_model(row1=row['layer0'].astype(int), row2=row['layer1'].astype(int), row3=row['layer2'].astype(int))
 
-		df_model.at[index, 'loss'] = loss_and_metrics[0]
-		df_model.at[index, 'acc'] = loss_and_metrics[1]
+        model.fit(x_train, training_labels, epochs=15, batch_size=128)
 
-	print(df_model)
+        loss_and_metrics = model.evaluate(x_test, testing_labels, batch_size=128)
+
+        df_model.at[index, 'loss'] = loss_and_metrics[0]
+        df_model.at[index, 'acc'] = loss_and_metrics[1]
+
+    print(df_model)
+    df_model.to_csv(output)
 
 
 if __name__ == '__main__':
